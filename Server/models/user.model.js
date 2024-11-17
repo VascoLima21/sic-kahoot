68f1723a8933af67266409ac717a3172a2954a5b
VascoLima21 <you@example.com>
2024-11-17 16:34:14 +0000

    User Login Working

--- Server/models/user.model.js
+++ Server/models/user.model.js
@@ -1,5 +1,6 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../../connection')
+const bcrypt = require('bcrypt')
 
 const User = sequelize.define("User",
     {
--- connection.js
+++ connection.js
@@ -7,9 +7,6 @@ const db_password = process.env.DB_PASSWORD
 const db_host = process.env.DB_HOST
 const db_dialect = 'mysql'
 
-console.log(db_name, db_user, db_password, db_host);
-
-
 const sequelize = new Sequelize(db_name, db_user, db_password, {
     host: db_host,
     dialect: db_dialect
