bd15f554e227977143baa166d78307428c2f498c
VascoLima21 <you@example.com>
2025-01-07 14:51:25 +0000

    Changes to Quizes controller and adding functions to play quizz

--- Server/models/quizzInstance.model.js
+++ Server/models/quizzInstance.model.js
@@ -1,22 +1,39 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../../connection')
+const sequelize = require('../../connection');
 
-const QuizzInstance = sequelize.define("QuizzInstance",
-    {
-        instanceId: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        date: {
-            type: DataTypes.DATE,
-            allowNull: false,
-        }
+const QuizzInstance = sequelize.define("QuizzInstance", {
+    instanceId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
     },
-    {
-        tableName: "QuizzInstances",
-    }
-);
+    roomCode: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    hostId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    date: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+    },
+    currentQuestionIndex: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+    },
+    status: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: 'waiting', // Possíveis valores: waiting, inProgress, completed
+    },
+}, {
+    tableName: "QuizzInstances",
+});
 
 module.exports = QuizzInstance;
